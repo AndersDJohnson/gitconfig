dde590691080e47fc442a7abaedfea692f4a062b
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-03-25 18:15:13 -0500

    add install.sh script

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+BASE=$(dirname $(readlink -f $0))
+
+# $1 = filename
+template() {
+  echo "$(eval "echo \"$(cat "$1")\"")"
+}
+
+## variables
+
+GITHUB_USER="AndersDJohnson"
+GIT_NAME="Anders D. Johnson"
+read -p "Git email: " GIT_EMAIL
+read -p "GitHub token: " GITHUB_TOKEN
+read -p "GitHub password: " GITHUB_PASSWORD
+
+## run
+
+cd "$BASE"
+
+ln -sf gitconfig ~/.gitconfig
+template "gitconfig-github" > ~/.gitconfig-github
+template "gitconfig-user" > ~/.gitconfig-user
