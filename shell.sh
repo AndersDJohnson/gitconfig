d635ceaaf21579dc5914603df75c9f95c815e3c2
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2015-08-30 16:19:41 -0500

    Dynamically use OSX keychain credentials, if available.

--- shell.sh
+++ shell.sh
@@ -4,3 +4,11 @@ function gi() {
   curl -L -s https://www.gitignore.io/api/$@ ;
 }
 
+
+git help -a | grep credential-osxkeychain &> /dev/null
+if [ $? -eq 0 ]; then
+  git config --file gitconfig-dynamic credential.helper osxkeychain
+else
+  git config --file gitconfig-dynamic credential-helper cache
+fi
+
